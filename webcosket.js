a12ee5863df34e31d22b5402ddbd4c81e2bf9388
张少林-kde <zsl@kuba.bz>
2016-04-22 03:15:06 +0800

    change: 使用开源的session模块

--- webcosket.js
+++ webcosket.js
@@ -4,10 +4,10 @@ if (typeof loadLib === 'undefined') {
 }
 const path = loadLib('path');
 const Event = loadLib('./lib/event');
+const session = loadLib('koa-socket-session');
 const IO = require('koa-socket');
 const cutils = loadLib('cutils');
-const Cookies = require('cookies');
-const compose = loadLib('koa-compose');
+const co = cutils.co;
 
 exports = module.exports = class WebSocket {
 	constructor(app, options) {
@@ -21,7 +21,6 @@ exports = module.exports = class WebSocket {
 		} else {
 			io = new IO();
 		}
-		this.middleware = [];
 		this.dir = options['dir'];
 		this.session = options['session'];
 		this.config = options['config'];
@@ -36,23 +35,31 @@ exports = module.exports = class WebSocket {
 		io.on('disconnect', ctx => {
 			console.log('Leave event', ctx.socket.id);
 		});
+
+		io.use(session(app, app.session));
+
+		cutils.each(this.config, (k, v)=> {
+			let event = loadLib(path.join(this.dir, v));
+			io.on(k, co.wrap(function *(ctx, msg) {
+				yield event.onMessage(ctx, msg);
+			}));
+		});
+
 		app.websocket = this;
 	}
 
 	attach(server) {
 		this.session = this.app.session;
-		this.server = server;
 		this.app.server = server;
 		this.io.attach(this.app);
-		WebSocket.initEvents.call(this);
 	}
 
 	broadcast(event, msg) {
 		this.io.broadcast(event, msg);
 	}
 
-	use(func) {
-		this.middleware.push(func);
+	use(middleware) {
+		this.io.use(wrap(middleware));
 		return this;
 	}
 
@@ -64,45 +71,16 @@ exports = module.exports = class WebSocket {
 		return new Event();
 	}
 
-	static initEvents() {
-		cutils.each(this.config, (k, v)=> {
-			let event = loadLib(path.join(this.dir, v));
-			this.io.on(k, (packet, data) => {
-				WebSocket.packetHandler.call(this, event, packet, data);
-			});
-		});
-	}
-
-	static packetHandler(event, packet, data) {
-		const $this = this;
-		cutils.ao(function*() {
-			yield WebSocket.parseSession.call($this, packet);
-			if ($this.middleware.length > 0) {
-				yield compose($this.middleware).call(packet);
-			}
-			yield event.onMessage(packet, data);
-		}, function (err) {
-			console.log(err);
-			if (err) {
-				throw err;
-			}
-		});
-	}
-
-	static *parseSession(packet) {
-		let socket = packet.socket.socket;
-		packet.url = socket.request.url;
-		if (!packet.cookies) {
-			packet.cookies = new Cookies(socket.handshake, socket.handshake, {
-				keys: this.app.keys,
-				secure: socket.handshake.secure
-			});
-		}
-		yield this.session.call(packet, WebSocket.nup());
-	}
-
-	static *nup() {
-	}
 };
+
+function wrap(middleware) {
+	return co.wrap(function *(ctx, next) {
+		try {
+			yield middleware.call(ctx, next);
+		} catch (e) {
+			console.log(e);
+		}
+	});
+}
 
 
