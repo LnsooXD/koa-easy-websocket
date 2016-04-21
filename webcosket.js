097bb61798a30ca3d18756cbf05ea3e48bf95253
张少林-kde <zsl@kuba.bz>
2016-04-22 00:13:55 +0800

    change: 使用class

--- webcosket.js
+++ webcosket.js
@@ -9,17 +9,8 @@ const cutils = loadLib('cutils');
 const Cookies = require('cookies');
 const compose = loadLib('koa-compose');
 
-exports = module.exports = cutils.class({
-	namespace: null,
-	app: null,
-	io: null,
-	session: null,
-	dir: null,
-	config: null,
-	userCache: {},
-	server: null,
-	middleware: [],
-	init: function (app, options) {
+exports = module.exports = class WebSocket {
+	constructor(app, options) {
 		if (cutils.is.nullOrUndefined(options)) {
 			options = {};
 		}
@@ -30,6 +21,7 @@ exports = module.exports = cutils.class({
 		} else {
 			io = new IO();
 		}
+		this.middleware = [];
 		this.dir = options['dir'];
 		this.session = options['session'];
 		this.config = options['config'];
@@ -44,75 +36,73 @@ exports = module.exports = cutils.class({
 		io.on('disconnect', ctx => {
 			console.log('Leave event', ctx.socket.id);
 		});
-
 		app.websocket = this;
 	}
-});
 
-exports.fn.attach = function (server) {
-	this.session = this.app.session;
-	this.server = server;
-	this.app.server = server;
-	this.io.attach(this.app);
-	initEvents.call(this);
-};
+	attach(server) {
+		this.session = this.app.session;
+		this.server = server;
+		this.app.server = server;
+		this.io.attach(this.app);
+		WebSocket.initEvents.call(this);
+	}
 
-exports.fn.broadcast = function (event, msg) {
-	this.io.broadcast(event, msg);
-};
+	broadcast(event, msg) {
+		this.io.broadcast(event, msg);
+	}
 
-exports.fn.use = function (func) {
-	this.middleware.push(func);
-	return this;
-};
+	use(func) {
+		this.middleware.push(func);
+		return this;
+	}
 
-exports.newWebSocket = (app, options) => {
-	return new exports(app, options);
-};
+	static newWebSocket(app, options) {
+		return new WebSocket(app, options);
+	}
 
-exports.newEvent = ()=> {
-	return new Event();
-};
+	static newEvent() {
+		return new Event();
+	}
 
-function initEvents() {
-	cutils.each(this.config, (k, v)=> {
-		let event = loadLib(path.join(this.dir, v));
-		this.io.on(k, (packet, data) => {
-			packetHandler.call(this, event, packet, data);
-		});
-	});
-}
-
-function packetHandler(event, packet, data) {
-	const $this = this;
-	cutils.ao(function*() {
-		yield parseSession.call($this, packet);
-		if ($this.middleware.length > 0) {
-			yield compose($this.middleware).call(packet);
-		}
-		yield event.onMessage(packet, data);
-	}, function (err) {
-		console.log(err);
-		if (err) {
-			throw err;
-		}
-	});
-}
-
-function *parseSession(packet) {
-	let socket = packet.socket.socket;
-	packet.url = socket.request.url;
-	if (!packet.cookies) {
-		packet.cookies = new Cookies(socket.handshake, socket.handshake, {
-			keys: this.app.keys,
-			secure: socket.handshake.secure
+	static initEvents() {
+		cutils.each(this.config, (k, v)=> {
+			let event = loadLib(path.join(this.dir, v));
+			this.io.on(k, (packet, data) => {
+				WebSocket.packetHandler.call(this, event, packet, data);
+			});
 		});
 	}
-	yield this.session.call(packet, nup());
-}
 
-function *nup() {
-}
+	static packetHandler(event, packet, data) {
+		const $this = this;
+		cutils.ao(function*() {
+			yield WebSocket.parseSession.call($this, packet);
+			if ($this.middleware.length > 0) {
+				yield compose($this.middleware).call(packet);
+			}
+			yield event.onMessage(packet, data);
+		}, function (err) {
+			console.log(err);
+			if (err) {
+				throw err;
+			}
+		});
+	}
 
+	static *parseSession(packet) {
+		let socket = packet.socket.socket;
+		packet.url = socket.request.url;
+		if (!packet.cookies) {
+			packet.cookies = new Cookies(socket.handshake, socket.handshake, {
+				keys: this.app.keys,
+				secure: socket.handshake.secure
+			});
+		}
+		yield this.session.call(packet, WebSocket.nup());
+	}
+
+	static *nup() {
+	}
+};
 
 
